2710405def104ad4ac2df2d4483baafb7930ea71
zNotChill <znotchill@znci.dev>
2023-06-08 18:16:45 +0100

    Fix keypress spam with a cooldown queue

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 const game = new garlic(800, 900);
 
 const canvas = game.canvas;
-const player = game.player;
+const player = game.entity;
 function render() {
 	canvas.create();
 }
+
 function draw() {
 
 	canvas.clear();
 
 	canvas.drawBackground("#ffbff3");
-
-	canvas.drawText(`FPS: ${game.currentFramerate}`, 0, 32, "#fff", "32px monospace")
-
+	canvas.drawText(`FPS: ${game.currentFramerate}`, 0, 32, "#fff", "32px monospace");
 	canvas.cursor("/img/crosshair_invert.png");
+	canvas.drawRect(player.x, player.y, player.width, player.height, "#fff");
+
+	player.move(canvas.mouse.x, canvas.mouse.y);
 
 }
 render();
